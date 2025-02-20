599fbbc77c56b9709db8e2140bb74c0887680054
Vaishnavi-veni <vaishnavinr06@gmail.com>
2025-02-20 16:33:04 +0530

    homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:marquee/marquee.dart';
+import 'package:payansh/constants/app_colors.dart';
+import 'package:payansh/constants/dimensions.dart';
 import 'package:payansh/routes/routes.dart';
 import 'package:payansh/widgets/app_bar.dart';
 import '../utils/local_storage.dart';
@@ -37,7 +41,8 @@ class HomeScreen extends StatelessWidget {
                 CircleAvatar(
                   radius: 24,
                   backgroundColor: Colors.white,
-                  child: Text("TU", style: TextStyle(fontWeight: FontWeight.bold)),
+                  child:
+                      Text("TU", style: TextStyle(fontWeight: FontWeight.bold)),
                 ),
                 const SizedBox(width: 10),
                 Column(
@@ -45,7 +50,10 @@ class HomeScreen extends StatelessWidget {
                   children: const [
                     Text(
                       "Test User",
-                      style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
                     ),
                     Text(
                       "Welcome to Payance!",
@@ -58,13 +66,103 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-
           // Main Content Below
-          
+          Positioned.fill(
+              top: 180,
+              left: 20,
+              right: 20,
+              child: Column(
+                children: [
+                  NoticeWidget(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 180, // Adjust height based on image size
+                    child: PageView(
+                      children: [
+                        SvgPicture.asset(
+                          "assets/banner/banner-1.svg",
+                          fit: BoxFit.fill,
+                        ),
+                        SvgPicture.asset(
+                          "assets/banner/sBanner-2.svg",
+                          fit: BoxFit.fill,
+                        ),
+                        Image.asset("assets/banner/Banner-3.png",
+                            fit: BoxFit.fill),
+                        SvgPicture.asset("assets/banner/sBanner-4.svg",
+                            fit: BoxFit.fill),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SvgPicture.asset(
+                    "assets/banner/Link-Bank-Banner.svg",
+                    width: 500, // Adjust width as needed
+                    height: 170, // Adjust height as needed
+                    fit: BoxFit
+                        .fill, // Ensures the image covers the space properly
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SvgPicture.asset("assets/frame/Frame_recharge.svg"),
+                      SvgPicture.asset("assets/frame/view_all.svg"),
+                    ],
+                  )
+                ],
+              ))
         ],
       ),
     );
   }
+}
 
- 
+class NoticeWidget extends StatelessWidget {
+  const NoticeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: Dimensions.dynamicHeight(context, 0.05),
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(color: AppColors.gradientEnd, width: 1)),
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(colors: [
+              AppColors.gradientStart.withOpacity(0.35),
+              Colors.white
+            ])),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: 20, // Adjust height as needed
+                child: Marquee(
+                  text:
+                      "Complete your KYC to avail bill payment and other services",
+                  style: TextStyle(fontSize: 14),
+                  scrollAxis: Axis.horizontal,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  blankSpace: 20.0,
+                  velocity: 30.0,
+                  pauseAfterRound: Duration(seconds: 1),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Image.asset(
+              "assets/icon_image/customer.png",
+              width: 30,
+            ),
+          ],
+        ));
+  }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:payansh/controllers/auth_controller.dart';
+import 'package:payansh/screens/device_info.dart';
 import 'package:payansh/screens/forgot_password.dart';
 import 'package:payansh/screens/register.dart';
 import 'package:payansh/theme/custom_themes/text_theme.dart';
@@ -104,7 +106,9 @@ class LoginScreen extends StatelessWidget {
               const SizedBox(height: 10),
 
               // Login with OTP
-              GradientButton(text: "Login with OTP", onPressed: () {}),
+              GradientButton(text: "Login with OTP", onPressed: () {
+                Get.to(() =>  DeviceInfoScreen());
+              }),
 
               const SizedBox(height: 10),
               Column(
