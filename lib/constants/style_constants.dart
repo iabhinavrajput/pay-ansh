695ae5f6733136e8901bc061cc9d793800479d73
iabhinavrajput <abhinavrajput207@gmail.com>
2025-02-28 14:41:52 +0530

    style constant files added

---
+++ lib/constants/style_constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class StyleConstants {
+  static const TextStyle whiteTextBold = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle profileNameText = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle profilePhoneText = TextStyle(
+    color: Colors.white,
+  );
+
+  static const TextStyle badgeText = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+}
