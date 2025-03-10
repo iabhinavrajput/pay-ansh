1c1574317d1c886855cd87c479d7e9dc4fb86a3c
iabhinavrajput <abhinavrajput207@gmail.com>
2025-03-10 10:53:23 +0530

    routes fixed for homescreen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -22,7 +22,7 @@ class AppRoutes {
     GetPage(
         name: otp,
         page: () => OtpVerification(userId: 123)), // Pass userId dynamically
-    GetPage(name: home, page: () =>  RechargeBillPage()),
+    // GetPage(name: home, page: () =>  RechargeBillPage()),
     GetPage(name: home, page: () =>  const HomeScreen()),
     GetPage(name: drawerNavigation, page :() =>   DrawerNavigation()),
     GetPage(name: profileScreen, page: () => const ProfileScreen()),
