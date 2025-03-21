720d8e2f2817963dd33cf65260272a1a5bcc9532
iabhinavrajput <abhinavrajput207@gmail.com>
2025-03-21 15:47:39 +0530

    kyc pan

---
+++ lib/screens/kyc/kyc_4.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:payansh/components/custom_app_bar_kyc.dart';
+import 'package:payansh/constants/app_colors.dart';
+
+class KycFour extends StatelessWidget {
+  const KycFour({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const CustomAppBarKYC(
+        title: "KYC by Pan",
+        description: "Please note that your PAN number needs to be verified during KYC verification",
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "KYC by PAN",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Enter Your Pan Card Number",
+                 labelText: 'Pan Card Number',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  
+                ),
+                
+              ),
+              
+            ),
+            const SizedBox(height: 20),
+            const Center(child: Text("or" , style: TextStyle(color: Colors.grey))),
+            const SizedBox(height: 20),
+            const Text(
+              "Upload Your PAN Card*",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+              // InputDecoration(border: OutlineInputBorder(), labelText: 'Password'),
+            ),
+            const SizedBox(height: 10),
+            _buildUploadBox("Upload PAN Card (Front)"),
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUploadBox(String text) {
+    return DottedBorder(
+      borderType: BorderType.RRect,
+      radius: const Radius.circular(10),
+      dashPattern: const [5, 5],
+      color: Colors.grey,
+      strokeWidth: 1.5,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
